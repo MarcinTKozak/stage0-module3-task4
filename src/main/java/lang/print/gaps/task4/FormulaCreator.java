2149d33b48641328d52bfe02801f96627da1f8f4
Marcin <marcin.t.kozak@gmail.com>
2023-06-07 21:35:24 +0100

    commit 1

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.task4;
 
 public class FormulaCreator {
+    private static int b;
+
     public static void main(String[] args) {
+
+        int a = 2;
+        int b = 4;
+
+        double score = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        System.out.println(score);
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+
+        char[] chars = {c, h, a, r};
+        for (char o : chars) {
+            int intValue = (int) o;
+            System.out.println(intValue);
+        }
     }
 }
